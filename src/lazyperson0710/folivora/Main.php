fcbdd55074dd6bd3511ada8d2304fb9795a2d76b
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-04-06 23:07:54 +0900

    ServerTaskに関する基盤を作成

--- src/lazyperson0710/folivora/Main.php
+++ src/lazyperson0710/folivora/Main.php
@@ -4,28 +4,16 @@ declare(strict_types = 1);
 
 namespace lazyperson0710\folivora;
 
-use lazyperson0710\folivora\event\SampleEventListener;
-use lazyperson0710\folivora\utils\Error\EventListenerNotFoundException;
-use lazyperson0710\folivora\utils\ListenerList;
-use pocketmine\event\Listener;
+use lazyperson0710\folivora\module\task\ServerTask;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase {
 
-	public function onEnable() : void {
-		$this->registerEventListeners(
-			new SampleEventListener(),
-		);
+	protected function onEnable() : void {
+		ServerTask::getInstance()->init($this->getScheduler());
 	}
 
-	private function registerEventListeners(ListenerList ...$list) : void {
-		foreach ($list as $listener) {
-			foreach ($listener->getListeners() as $event) {
-				if (!$event instanceof Listener) throw new EventListenerNotFoundException("変数に格納されたクラスはListenerを実装していません");
-				$this->getServer()->getPluginManager()->registerEvents($event, $this);
-			}
-		}
+	protected function onDisable() : void {
 	}
 
-	//note https://github.com/mmm545/PacketLogger/blob/6e289848dcd0d4ee3633908d54d3a4e19d3b53db/src/PacketLogger.php
 }
