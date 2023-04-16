d5ec223caede5c22fa1080334b2ee585adbf8cb7
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-04-16 17:49:51 +0900

    起動時にdataPathをメモリに保存するように

--- src/lazyperson0710/folivora/Folivora.php
+++ src/lazyperson0710/folivora/Folivora.php
@@ -4,11 +4,13 @@ declare(strict_types = 1);
 
 namespace lazyperson0710\folivora;
 
+use lazyperson0710\folivora\util\config\ConfigDataPath;
 use pocketmine\plugin\PluginBase;
 
 class Folivora extends PluginBase {
 
 	protected function onEnable() : void {
+		ConfigDataPath::getInstance()->setPath($this->getDataFolder());
 	}
 
 	protected function onDisable() : void {
