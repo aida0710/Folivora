e261235bf73fc885b81a9a307c8d737a10ec6c4b
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-05-09 02:02:02 +0900

    dataPathを簡単に取得できるように

--- src/lazyperson0710/folivora/Folivora.php
+++ src/lazyperson0710/folivora/Folivora.php
@@ -4,16 +4,22 @@ declare(strict_types = 1);
 
 namespace lazyperson0710\folivora;
 
-use lazyperson0710\folivora\util\config\ConfigDataPath;
 use pocketmine\plugin\PluginBase;
+use RuntimeException;
 
 class Folivora extends PluginBase {
 
-	protected function onEnable() : void {
-		ConfigDataPath::getInstance()->setPath($this->getDataFolder());
-	}
+    private static string $dataPath;
+
+    protected function onEnable() : void {
+        self::$dataPath = $this->getDataFolder();
+    }
 
 	protected function onDisable() : void {
 	}
 
+    public static function getDataPath() : string {
+        if (!isset(self::$dataPath)) throw new RuntimeException("Path is not set.");
+        return self::$dataPath;
+    }
 }
