32cf3970a02722e8ab55ab3fb0d52f10f48560ff
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-05-11 02:09:28 +0900

    コマンド看板を追加

---
+++ src/lazyperson0710/folivora/features/other/listener/CommandSign.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace lazyperson0710\folivora\features\other\listener;
+
+use lazyperson710\core\packet\SendMessage\SendActionBarMessage;
+use lazyperson710\core\packet\SendNoSoundMessage\SendNoSoundTip;
+use lazyperson710\core\task\IntervalTask;
+use pocketmine\block\BaseSign;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\Server;
+use function array_keys;
+use function array_values;
+use function floor;
+use function str_replace;
+
+class CommandSign implements Listener {
+
+    /**
+     * @priority LOWEST
+     */
+    public function onTap(PlayerInteractEvent $event) : void {
+        $block = $event->getBlock();
+        if ($block instanceof BaseSign) {
+            if ($block->getText()->getLine(0) == '##cmd') {
+                if (!$event->getPlayer()->isSneaking()) {
+                    if (IntervalTask::check($event->getPlayer(), 'CmdSigns')) {
+                        SendNoSoundTip::Send($event->getPlayer(), 'コマンド看板は1秒以内に再度使用することは出来ません', 'CmdSigns', true);
+                        return;
+                    } else {
+                        IntervalTask::onRun($event->getPlayer(), 'CmdSigns', 20);
+                    }
+                    $player = $event->getPlayer();
+                    $world = $player->getWorld()->getFolderName();
+                    $floor_x = floor($player->getPosition()->getX());
+                    $floor_y = floor($player->getPosition()->getY());
+                    $floor_z = floor($player->getPosition()->getZ());
+                    $table = [
+                        '{name}' => $player->getName(),
+                        '{x}' => $floor_x,
+                        '{y}' => $floor_y,
+                        '{z}' => $floor_z,
+                        '{world}' => $world,
+                    ];
+                    $search = array_keys($table);
+                    $replace = array_values($table);
+                    $cmd = $block->getText()->getLine(2);
+                    Server::getInstance()->dispatchCommand($event->getPlayer(), str_replace($search, $replace, $cmd));
+                } else {
+                    SendActionBarMessage::Send($event->getPlayer(), 'スニーク中はコマンド看板は実行されず破壊が可能です', 'CmdSigns', true);
+                }
+            }
+        }
+    }
+}
