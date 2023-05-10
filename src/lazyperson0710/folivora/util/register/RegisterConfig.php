cd37fa54d766f9dbf5dc8a6302281f25d7a3e732
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-05-11 04:41:28 +0900

    Configのデータパスクラスを追加

---
+++ src/lazyperson0710/folivora/util/register/RegisterConfig.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace lazyperson0710\folivora\util\register;
+
+use lazyperson0710\folivora\util\exception\ConfigDataPathNotSetException;
+
+class RegisterConfig {
+
+    private static string $dataPath;
+
+    /**
+     * Configを保存するデータフォルダのパスを取得します。
+     *
+     * @return string
+     */
+    public static function getDataPath() : string {
+        if (!isset(self::$dataPath)) throw new ConfigDataPathNotSetException("Path is not set.");
+        return self::$dataPath;
+    }
+
+    /**
+     * プラグイン起動時に一度だけ実行してください。
+     *
+     * @param string $path
+     * @return void
+     */
+    public static function setDataPath(string $path) : void {
+        self::$dataPath = $path;
+    }
+
+}
