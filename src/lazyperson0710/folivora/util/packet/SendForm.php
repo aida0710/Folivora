ea425ec9946df2f94b7bf61f88ead4043c6784ce
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-05-11 02:05:23 +0900

    Form送信時の統一クラスを作成

---
+++ src/lazyperson0710/folivora/util/packet/SendForm.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace lazyperson0710\folivora\util\packet;
+
+use lazyperson0710\folivora\util\message\send_soundless_message\SendSoundlessTip;
+use lazyperson710\core\task\IntervalTask;
+use pocketmine\form\Form;
+use pocketmine\player\Player;
+
+class SendForm {
+
+    /**
+     * 基本的にformをプレイヤーに送信するときは使ってください
+     *
+     * @param Player $player
+     * @param Form   $form
+     * @return void
+     */
+    public static function Send(Player $player, Form $form) : void {
+        if (IntervalTask::check($player, 'SendForm')) {
+            SendSoundlessTip::Send($player, '0.3秒以内連続でFormを送信することは出来ません', 'SendForm', true);
+            return;
+        } else {
+            IntervalTask::onRun($player, 'SendForm', 6);
+        }
+        $player->sendForm($form);
+    }
+}
