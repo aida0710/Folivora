49e0bc804966e913b5a81b5571de17c927597db2
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-05-11 02:08:44 +0900

    RedStoneOreBlockの挙動を変更

---
+++ src/lazyperson0710/folivora/features/other/overwrite/block/RedStoneOreBlock.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace lazyperson0710\folivora\features\other\Overwrite\block;
+
+use pocketmine\block\RedstoneOre;
+use pocketmine\item\Item;
+use pocketmine\math\Vector3;
+use pocketmine\player\Player;
+
+class RedStoneOreBlock extends RedstoneOre {
+
+    protected bool $lit = false;
+
+    public function readStateFromData(int $id, int $stateMeta) : void {
+        $this->lit = $id === $this->idInfoFlattened->getSecondId();
+    }
+
+    public function isLit() : bool {
+        return $this->lit;
+    }
+
+    /**
+     * @return $this
+     */
+    public function setLit(bool $lit = true) : self {
+        return $this;
+    }
+
+    public function getLightLevel() : int {
+        return 0;
+    }
+
+    public function onInteract(Item $item, int $face, Vector3 $clickVector, ?Player $player = null) : bool {
+        return false;
+    }
+
+    public function onNearbyBlockChange() : void {
+    }
+
+    public function ticksRandomly() : bool {
+        return false;
+    }
+
+}
