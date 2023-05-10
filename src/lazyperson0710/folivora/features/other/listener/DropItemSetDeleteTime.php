cb9b4b7d64adc3ec39428c1dafd0faf798e5ccd5
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-05-11 02:09:55 +0900

    ドロップしたアイテムを1分で消えるように

---
+++ src/lazyperson0710/folivora/features/other/listener/DropItemSetDeleteTime.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace lazyperson0710\folivora\features\other\listener;
+
+use pocketmine\entity\object\ItemEntity;
+use pocketmine\event\entity\ItemSpawnEvent;
+use pocketmine\event\Listener;
+
+class DropItemSetDeleteTime implements Listener {
+
+    private const DeleteTime = 20 * 60;
+
+    public function onDropItemSetDeleteTime(ItemSpawnEvent $event) : void {
+        if ($event->getEntity() instanceof ItemEntity) {
+            $event->getEntity()->setDespawnDelay(self::DeleteTime);
+        }
+    }
+
+}
