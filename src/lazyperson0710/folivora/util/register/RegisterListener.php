8ee73d3ea3e5d0d576f00313229e127b83e690b9
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-05-11 04:42:32 +0900

    コメントを追加し、アクセス修飾子を修正

--- src/lazyperson0710/folivora/util/register/RegisterListener.php
+++ src/lazyperson0710/folivora/util/register/RegisterListener.php
@@ -9,16 +9,37 @@ class RegisterListener {
 
     private static Plugin $plugin;
 
+    /**
+     * プラグイン起動時に一度だけ実行してください。
+     *
+     * @param Plugin $plugin
+     * @return void
+     */
     public static function setPlugin(Plugin $plugin) : void {
         self::$plugin = $plugin;
     }
 
-    public static function getPlugin() : Plugin {
-        return self::$plugin;
+    /**
+     * イベントListenerを登録します。
+     * 起動時に読み込まれるクラスに記述するようにしてください。@param Listener $listener
+     *
+     * @return void
+     * @see RegisterFeatures::enableFeatures()
+     *
+     */
+    public static function register(Listener $listener) : void {
+        $registerListener = new RegisterListener();
+        $plugin = $registerListener->getPlugin();
+        $plugin->getServer()->getPluginManager()->registerEvents($listener, $plugin);
     }
 
-    public static function register(Listener $listener) : void {
-        self::getPlugin()->getServer()->getPluginManager()->registerEvents($listener, self::getPlugin());
+    /**
+     * Pluginオブジェクトを取得します。
+     *
+     * @return Plugin
+     */
+    private function getPlugin() : Plugin {
+        return self::$plugin;
     }
 
 }
