aa9cfc2642d111860b72cc32fd2941ddb700d240
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-05-11 04:43:40 +0900

    処理をまとめることで不快感を削減

--- src/lazyperson0710/folivora/Folivora.php
+++ src/lazyperson0710/folivora/Folivora.php
@@ -4,28 +4,22 @@ declare(strict_types = 1);
 
 namespace lazyperson0710\folivora;
 
+use lazyperson0710\folivora\util\register\RegisterConfig;
 use lazyperson0710\folivora\util\register\RegisterFeatures;
 use lazyperson0710\folivora\util\register\RegisterListener;
-use lazyperson0710\folivora\util\register\RegisterTask;
+use lazyperson0710\folivora\util\register\RegisterTaskScheduler;
 use pocketmine\plugin\PluginBase;
-use RuntimeException;
 
 class Folivora extends PluginBase {
 
-    private static string $dataPath;
-
     protected function onEnable() : void {
-        self::$dataPath = $this->getDataFolder();
+        RegisterConfig::setDataPath($this->getDataFolder());
         RegisterListener::setPlugin($this);
-        RegisterTask::init($this->getScheduler());
+        RegisterTaskScheduler::init($this->getScheduler());
         RegisterFeatures::enableFeatures($this->getServer());
     }
 
     protected function onDisable() : void {
-    }
-
-    public static function getDataPath() : string {
-        if (!isset(self::$dataPath)) throw new RuntimeException("Path is not set.");
-        return self::$dataPath;
+        RegisterFeatures::disableFeatures($this->getServer());
     }
 }
