379b6aa97c78734fe64d28b59fa26298365bb5a5
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-05-11 04:41:53 +0900

    disableFeaturesを追加し冗長な確認処理を削除

--- src/lazyperson0710/folivora/util/register/RegisterFeatures.php
+++ src/lazyperson0710/folivora/util/register/RegisterFeatures.php
@@ -4,29 +4,61 @@ declare(strict_types = 1);
 
 namespace lazyperson0710\folivora\util\register;
 
-use lazyperson0710\folivora\features\other\Other;
+use lazyperson0710\folivora\features\debug\Debug;
 use lazyperson0710\folivora\util\plugin_base\IPluginBase;
 use pocketmine\Server;
 
 class RegisterFeatures {
 
+    /** @var IPluginBase[] */
+    private array $features = [];
+
+    /**
+     * 仕組み上、登録した順に読み込みますので優先度調整に気を付けてください。
+     *
+     * @param Server $server
+     * @return void
+     */
     public static function enableFeatures(Server $server) : void {
-        //todo 優先順位を指定できるようにする
-        $class = [
-            new Other(),
-        ];
-        foreach ($class as $pluginBase) {
-            if ($pluginBase instanceof IPluginBase) {
-                (new RegisterFeatures)->register($pluginBase, $server);
-            } else {
-                throw new \RuntimeException("Class must be instance of IPluginBase.");
-            }
+        $registerFeatures = new RegisterFeatures();
+        $registerFeatures->setFeatures(new Debug());
+        foreach ($registerFeatures->getFeatures() as $pluginBase) {
+            $pluginBase->onEnable($server);
         }
     }
 
-    private function register(IPluginBase $pluginBase, Server $server) : void {
-        $pluginBase->onEnable($server);
-        $pluginBase->onDisable($server);
+    /**
+     * 登録した順にdisableを実行します。
+     * 実行順を変更したい場合は配列を操作して下さい。@param Server $server
+     *
+     * @return void
+     * @see $features
+     *
+     */
+    public static function disableFeatures(Server $server) : void {
+        $registerFeatures = new RegisterFeatures();
+        foreach ($registerFeatures->getFeatures() as $pluginBase) {
+            $pluginBase->onDisable($server);
+        }
+    }
+
+    /**
+     * 追加された機能を取得します。
+     *
+     * @return IPluginBase[]
+     */
+    public function getFeatures() : array {
+        return $this->features;
+    }
+
+    /**
+     * 追加はこの関数のみで行ってください。
+     *
+     * @param IPluginBase $pluginBase
+     * @return void
+     */
+    private function setFeatures(IPluginBase $pluginBase) : void {
+        $this->features[] = $pluginBase;
     }
 
 }
