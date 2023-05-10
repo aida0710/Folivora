3503c56ffc9d7d0ffbb5c191be3eda5822e9fc27
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-05-11 02:11:38 +0900

    pvpワールド以外だとダメージを食らわないように、エンティティ落下時にパーティクルを表示するように

---
+++ src/lazyperson0710/folivora/features/other/listener/HitEntityDelete.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace lazyperson0710\folivora\features\other\listener;
+
+use pocketmine\color\Color;
+use pocketmine\event\entity\ProjectileHitEvent;
+use pocketmine\event\Listener;
+use pocketmine\world\particle\DustParticle;
+
+class HitEntityDelete implements Listener {
+
+    public function onHit(ProjectileHitEvent $event) : void {
+        for ($i = 0; $i <= 0.6; $i += 0.2) {
+            $event->getEntity()->getPosition()->getWorld()->addParticle($event->getEntity()->getPosition()->add(0, $i, 0), new DustParticle(new Color(255, 255, 255)));
+        }
+        if ($event->getEntity()->getWorld()->getFolderName() === 'pvp') {
+            return;
+        }
+        $entity = $event->getEntity();
+        $entity->kill();
+    }
+
+}
