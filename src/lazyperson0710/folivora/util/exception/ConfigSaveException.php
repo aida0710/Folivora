45ffff3df1c5f2886e46689026e58240462d2d9e
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-05-14 23:46:45 +0900

    セーブ時のエラーハンドリング用クラスを作成

---
+++ src/lazyperson0710/folivora/util/exception/ConfigSaveException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace lazyperson0710\folivora\util\exception;
+
+use RuntimeException;
+
+class ConfigSaveException extends RuntimeException {
+
+    //null
+}
