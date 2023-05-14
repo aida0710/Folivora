be5504a6333bf25d7e7a697a26692e23056a09ba
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-05-14 23:47:26 +0900

    configを一括保存するように

--- src/lazyperson0710/folivora/util/register/RegisterConfig.php
+++ src/lazyperson0710/folivora/util/register/RegisterConfig.php
@@ -4,10 +4,16 @@ declare(strict_types = 1);
 
 namespace lazyperson0710\folivora\util\register;
 
+use JsonException;
+use lazyperson0710\folivora\util\config\IConfig;
 use lazyperson0710\folivora\util\exception\ConfigDataPathNotSetException;
+use lazyperson0710\folivora\util\exception\ConfigSaveException;
+use pocketmine\utils\Config;
 
 class RegisterConfig {
 
+    /** @var Config[] */
+    private static array $configList = [];
     private static string $dataPath;
 
     /**
@@ -30,5 +36,38 @@ class RegisterConfig {
      */
     public static function setDataPath(string $path) : void {
         self::$dataPath = $path;
+    }
+
+    /**
+     * 最初に必ずconfigをすべて登録してください。
+     *
+     * @param Config $config
+     * @return void
+     */
+    public static function registerConfig(Config $config) : void {
+        self::$configList = [];
+    }
+
+    /**
+     * 一括セーブなどにご利用ください。
+     *
+     * @return Config[]
+     */
+    public static function getRegisterConfig() : array {
+        return self::$configList;
+    }
+
+    /**
+     * @return void
+     * @throws JsonException
+     */
+    public static function saveAllConfig() : void {
+        foreach (self::$configList as $config) {
+            try {
+                $config->save();
+            } catch (ConfigSaveException $error) {
+                throw new ConfigSaveException($error->getMessage());
+            }
+        }
     }
 }
