b8ea77ee2a73e600ae9fc417ac295eed56e02d9b
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-05-16 22:47:52 +0900

    formクラスの原型だけ作成

---
+++ src/lazyperson0710/folivora/features/electronic_money/form/MoneyForm.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace lazyperson0710\folivora\features\electronic_money\form;
+
+use bbo51dog\bboform\form\SimpleForm;
+
+class MoneyForm extends SimpleForm {
+
+    public function __construct() {
+    }
+
+}
