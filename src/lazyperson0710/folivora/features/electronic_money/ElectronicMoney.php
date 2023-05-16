13279462e37dc2058d96d73bd255627cac483456
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-05-16 22:47:14 +0900

    IPluginBaseを実装。初期化処理など諸々書いた

--- src/lazyperson0710/folivora/features/electronic_money/ElectronicMoney.php
+++ src/lazyperson0710/folivora/features/electronic_money/ElectronicMoney.php
@@ -4,11 +4,33 @@ declare(strict_types = 1);
 
 namespace lazyperson0710\folivora\features\electronic_money;
 
-use pocketmine\plugin\PluginBase;
+use JsonException;
+use lazyperson0710\folivora\features\electronic_money\command\MoneyCommand;
+use lazyperson0710\folivora\features\electronic_money\listener\JoinEvent;
+use lazyperson0710\folivora\util\config\exception\ConfigSaveException;
+use lazyperson0710\folivora\util\plugin_base\IPluginBase;
+use lazyperson0710\folivora\util\register\RegisterListener;
+use pocketmine\Server;
 
-class ElectronicMoney {
+class ElectronicMoney implements IPluginBase {
 
-    public function init(PluginBase $base) : void {
+    /**
+     * @param Server $server
+     * @return void
+     * @throws JsonException
+     */
+    public function onEnable(Server $server) : void {
+        try {
+            Money::getInstance()->createConfigFile();
+        } catch (ConfigSaveException $exception) {
+            $server->getLogger()->error($exception->getMessage());
+        }
+        Money::getInstance()->registerConfigClass();
+        RegisterListener::register(new JoinEvent());
+        $server->getCommandMap()->register(Money::PREFIX, new MoneyCommand());
+    }
+
+    public function onDisable(Server $server) : void {
     }
 
 }
