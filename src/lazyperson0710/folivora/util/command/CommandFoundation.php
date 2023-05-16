db19372a0d5782fb97cf6fb8f62087c23eaa054e
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-05-16 22:45:34 +0900

    コマンドの基底クラスを作成

---
+++ src/lazyperson0710/folivora/util/command/CommandFoundation.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace lazyperson0710\folivora\util\command;
+
+class CommandFoundation {
+
+    public const NON_PLAYER = '現在実行したコマンドはプレイヤーのみ実行可能です';
+
+}
