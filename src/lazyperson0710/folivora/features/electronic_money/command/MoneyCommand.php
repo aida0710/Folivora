4cf143cdc96ab38a017e46c8cdfd196726685f2e
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-05-16 22:49:15 +0900

    moneyコマンドの基盤を作成

---
+++ src/lazyperson0710/folivora/features/electronic_money/command/MoneyCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace lazyperson0710\folivora\features\electronic_money\command;
+
+use lazyperson0710\folivora\util\command\CommandFoundation;
+use lazyperson0710\folivora\util\packet\SendForm;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use pocketmine\Server;
+
+class MoneyCommand extends Command {
+
+    public function __construct() {
+        parent::__construct('pay', 'お金を送金します。');
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args) : bool {
+        if (!$sender instanceof Player) {
+            Server::getInstance()->getLogger()->warning(CommandFoundation::NON_PLAYER);
+            return false;
+        }
+        match ($args[0]) {
+            'set' => $sender->sendMessage('set'),
+            'get' => $sender->sendMessage('send'),
+            'add' => $sender->sendMessage('add'),
+            'reduce' => $sender->sendMessage('remove'),
+            default => SendForm::Send($sender,),
+        };
+        return true;
+    }
+
+}
