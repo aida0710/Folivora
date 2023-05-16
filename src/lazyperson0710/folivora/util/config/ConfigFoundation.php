d1cdfd4376078d2f85d18c08e1fc50953f12b151
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-05-16 22:45:15 +0900

    なんか色々やった

--- src/lazyperson0710/folivora/util/config/ConfigFoundation.php
+++ src/lazyperson0710/folivora/util/config/ConfigFoundation.php
@@ -5,29 +5,47 @@ declare(strict_types = 1);
 namespace lazyperson0710\folivora\util\config;
 
 use JsonException;
-use lazyperson0710\folivora\util\exception\ConfigSaveException;
-use lazyperson0710\folivora\util\register\RegisterConfig;
+use lazyperson0710\folivora\features\electronic_money\exception\AccountNotFound;
+use lazyperson0710\folivora\util\config\exception\ConfigDataPathNotSetException;
+use lazyperson0710\folivora\util\config\exception\ConfigSaveException;
+use lazyperson0710\folivora\util\register\RegisterTaskScheduler;
+use pocketmine\player\Player;
+use pocketmine\scheduler\ClosureTask;
 use pocketmine\utils\Config;
 
-abstract class ConfigFoundation {
+class ConfigFoundation {
+
+    /** @var IConfig[] $iConfigs */
+    private static array $iConfigs;
+    private static string $dataPath;
 
     /**
-     * データをセーブするときは必ず使ってください。
-     * また、この関数を実行する直前にキャッシュを削除しデータの重複を避けてください。
+     * 最初に一度だけ実行してください。
      *
-     * @param array  $data
-     * @param Config $config
-     * @return bool
-     * @throws JsonException
+     * @param string $path
+     * @return void
      */
-    public function saveConfig(array $data, Config $config) : bool {
-        $config->setAll($data);
-        try {
-            $config->save();
-        } catch (ConfigSaveException $error) {
-            return false;
+    public static function init(string $path) : void {
+        self::$dataPath = $path;
+        RegisterTaskScheduler::getScheduler()->scheduleRepeatingTask(
+            new ClosureTask(
+                function () : void {
+                    self::runAllSave();
+                }
+            ), 20 * 30,
+        );
+    }
+
+    /**
+     * 基本的にこちらから3分ごとに自動で全ての記録データが保存されます。
+     * 非同期処理には対応していません。
+     *
+     * @return void
+     */
+    public static function runAllSave() : void {
+        foreach (self::$iConfigs as $iConfig) {
+            $iConfig->runSave();
         }
-        return true;
     }
 
     /**
@@ -35,29 +53,63 @@ abstract class ConfigFoundation {
      * ディレクトリ階層は自動で生成されるため$path = ' dir1/dir2/file.txt 'のように指定してください。
      * また、この静的関数を使用した場合はRegisterConfig::registerConfig()に自動で登録されます。
      *
-     * @param string   $path
+     * @param string $path
      * @param int|null $fileType
      * @return Config
      * @throws JsonException
-     *
-     * @see RegisterConfig::registerConfig()
-     *
+     * @see ConfigFoundation::registerConfig()
      */
-    protected function createConfigFile(string $path, ?int $fileType = Config::JSON) : Config {
+    public static function createConfigFile(string $path, ?int $fileType = Config::JSON) : Config {
         $pathArray = explode('/', $path);
         $fileName = array_pop($pathArray);
         $directoryPath = null;
         foreach ($pathArray as $value) {
-            if (file_exists($directoryPath . $value) === false) mkdir($directoryPath . $value, 0777, true);
+            if (file_exists(ConfigFoundation::getDataPath() . $directoryPath . $value) === false) mkdir(ConfigFoundation::getDataPath() . $directoryPath . $value, 0777, true);
             $directoryPath .= $value . '/';
         }
-        $config = new Config(RegisterConfig::getDataPath() . $directoryPath . $fileName, $fileType);
+        $config = new Config(ConfigFoundation::getDataPath() . $directoryPath . $fileName, $fileType);
         try {
             $config->save();
-        } catch (ConfigSaveException $error) {
-            throw new ConfigSaveException($error->getMessage());
+        } catch (ConfigSaveException $exception) {
+            throw new ConfigSaveException($exception->getMessage());
         }
-        RegisterConfig::registerConfig($config);
         return $config;
     }
+
+    /**
+     * Configを保存するデータフォルダのパスを取得します。
+     *
+     * @return string
+     */
+    public static function getDataPath() : string {
+        if (!isset(self::$dataPath)) {
+            throw new ConfigDataPathNotSetException('コンフィグパスが設定されていません。');
+        }
+        return self::$dataPath;
+    }
+
+    /**
+     * Configを使用するクラスは絶対に登録してください。
+     * セーブされません。
+     *
+     * @param IConfig $iConfig
+     * @return void
+     */
+    public static function registerConfigClass(IConfig $iConfig) : void {
+        self::$iConfigs[] = $iConfig;
+    }
+
+    /**
+     * @param Player $player
+     * @param Config $config
+     * @return bool
+     */
+    public static function isAccountExist(Player $player, Config $config) : bool {
+        $player_name = $player->getName();
+        if (!$config->get($player_name)) {
+            throw new AccountNotFound($player_name . 'のデータが存在しないためサーバーを停止しました');
+        }
+        return true;
+    }
+
 }
