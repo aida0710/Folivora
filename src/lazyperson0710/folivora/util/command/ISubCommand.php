53137a8936d1505a62bd3775f4fea57a72deb495
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-05-16 22:45:51 +0900

    サブコマンド用のインターフェイスを追加

---
+++ src/lazyperson0710/folivora/util/command/ISubCommand.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace lazyperson0710\folivora\util\command;
+
+interface ISubCommand {
+
+    /**
+     * @return void
+     */
+    public function execute() : void;
+
+}
