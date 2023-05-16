fa33c7b14c964d87cef9616a5e7ab104cd185cb3
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-05-16 22:42:25 +0900

    Templateを作成

---
+++ src/lazyperson0710/folivora/util/Template/TemplateCommand.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace lazyperson0710\folivora\util\Template;
+
+use lazyperson0710\folivora\util\command\CommandFoundation;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use pocketmine\Server;
+
+class TemplateCommand extends Command {
+
+    public function __construct() {
+        parent::__construct('template', 'Template');
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args) : bool {
+        if (!$sender instanceof Player) {
+            Server::getInstance()->getLogger()->warning(CommandFoundation::NON_PLAYER);
+            return false;
+        }
+        return true;
+    }
+
+}
