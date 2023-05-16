93534366c09ef7aad07b0fafe3c2219009030a75
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-05-16 22:46:49 +0900

    money関係の基底クラスを作成

--- src/lazyperson0710/folivora/features/electronic_money/Money.php
+++ src/lazyperson0710/folivora/features/electronic_money/Money.php
@@ -4,39 +4,93 @@ declare(strict_types = 1);
 
 namespace lazyperson0710\folivora\features\electronic_money;
 
-use lazyperson0710\folivora\util\config\features\PlayerData;
+use JsonException;
+use lazyperson0710\folivora\util\config\ConfigFoundation;
+use lazyperson0710\folivora\util\config\exception\ConfigSaveException;
+use lazyperson0710\folivora\util\config\IConfig;
+use lazyperson0710\folivora\util\exception\QuantityLimitReached;
 use pocketmine\player\Player;
+use pocketmine\utils\Config;
 use pocketmine\utils\SingletonTrait;
 
-class Money {
+class Money implements IConfig {
 
     use SingletonTrait;
 
-    public const NAME = "Money";
+    private Config $config;
+
+    public const PREFIX = "Money";
     public const SUFFIX = '円';
     public const DEFAULT_CURRENCY = 1500;
+    public const PATH = 'player/money.json';
 
-    public function addMoney(Player $player, int $quantity) : void {
-        $currentMoney = $this->getMoney($player);
-        $money = $currentMoney + $quantity;
-        $this->setMoney($player, $money);
+    /**
+     * @return void
+     * @throws JsonException
+     */
+    public function createConfigFile() : void {
+        try {
+            $this->config = ConfigFoundation::createConfigFile(self::PATH);
+        } catch (ConfigSaveException $exception) {
+            throw new ConfigSaveException($exception->getMessage());
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function registerConfigClass() : void {
+        ConfigFoundation::registerConfigClass($this);
+    }
+
+    /**
+     * @return void
+     * @throws JsonException
+     */
+    public function runSave() : void {
+        $this->config->save();
+    }
+
+    /**
+     * @param Player $player
+     * @return void
+     */
+    public function createAccount(Player $player) : void {
+        $player_name = $player->getName();
+        if ($this->config->get($player_name)) return;
+        $this->config->set($player_name, self::DEFAULT_CURRENCY);
     }
 
     public function getMoney(Player $player) : int {
-        $playerData = PlayerData::getInstance()->getPlayerData($player);
-        return $playerData[self::NAME];
+        $player_name = $player->getName();
+        if (!ConfigFoundation::isAccountExist($player, $this->config)) return 0;
+        return $this->config->get($player_name);
     }
 
-    public function setMoney(Player $player, int $quantity) : void {
-        if ($quantity < 0) $quantity = 0;
-        PlayerData::getInstance()->setData($player, self::NAME, $quantity);
+    public function setMoney(Player $player, int $money) : void {
+        $player_name = $player->getName();
+        if (!ConfigFoundation::isAccountExist($player, $this->config)) return;
+        if (PHP_INT_MAX < $money) throw new QuantityLimitReached(QuantityLimitReached::MESSAGE);
+        $this->config->set($player_name, $money);
     }
 
-    public function reduceMoney(Player $player, int $quantity) : void {
-        $currentMoney = $this->getMoney($player);
-        $money = $currentMoney - $quantity;
-        if ($money < 0) $money = 0;
-        $this->setMoney($player, $money);
+    public function addMoney(Player $player, int $money) : void {
+        $player_name = $player->getName();
+        if (!ConfigFoundation::isAccountExist($player, $this->config)) return;
+        $money = $this->getMoney($player) + $money;
+        if (PHP_INT_MAX < $money) throw new QuantityLimitReached(QuantityLimitReached::MESSAGE);
+        $this->config->set($player_name, $money);
+    }
+
+    public function reduceMoney(Player $player, int $money) : void {
+        $player_name = $player->getName();
+        if (!ConfigFoundation::isAccountExist($player, $this->config)) return;
+        $money = $this->getMoney($player) - $money;
+        if (0 >= $money) {
+            var_dump("0以下のため、0に設定しました。");
+            $money = 0;
+        }
+        $this->config->set($player_name, $money);
     }
 
 }
