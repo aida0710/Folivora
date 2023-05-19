b6a73cdbffd329c4eafd0efce9b274b0de2c552b
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-05-19 19:08:03 +0900

    テスト用機能を削除

--- src/lazyperson0710/folivora/features/debug/Debug.php
+++ src/lazyperson0710/folivora/features/debug/Debug.php
@@ -4,15 +4,12 @@ declare(strict_types = 1);
 
 namespace lazyperson0710\folivora\features\debug;
 
-use lazyperson0710\folivora\features\debug\test\listener\BreakListener;
 use lazyperson0710\folivora\util\plugin_base\IPluginBase;
-use lazyperson0710\folivora\util\register\RegisterListener;
 use pocketmine\Server;
 
 class Debug implements IPluginBase {
 
     public function onEnable(Server $server) : void {
-        RegisterListener::register(new BreakListener());
     }
 
     public function onDisable(Server $server) : void {
