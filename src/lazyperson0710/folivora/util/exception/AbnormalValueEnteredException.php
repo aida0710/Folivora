3d33f25c87d88dcaa22d153cdeb2403d79fb2f30
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-05-24 16:37:19 +0900

    異常値が入力された場合のエラークラスを追加

---
+++ src/lazyperson0710/folivora/util/exception/AbnormalValueEnteredException.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace lazyperson0710\folivora\util\exception;
+
+use RuntimeException;
+
+class AbnormalValueEnteredException extends RuntimeException {
+
+    public const MESSAGE = "異常値が入力されました";
+
+}
