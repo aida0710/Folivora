7780faa40aea8d31ad7e5c8f7a124a43b1b996ca
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-05-30 11:23:57 +0900

    iPluginBaseを追加

--- src/lazyperson0710/folivora/features/shop/Shop.php
+++ src/lazyperson0710/folivora/features/shop/Shop.php
@@ -10,18 +10,22 @@ use lazyperson0710\folivora\features\shop\command\ItemShopCommand;
 use lazyperson0710\folivora\features\shop\database\EffectShopAPI;
 use lazyperson0710\folivora\features\shop\database\EnchantShopAPI;
 use lazyperson0710\folivora\features\shop\database\ItemShopAPI;
-use pocketmine\plugin\PluginBase;
+use lazyperson0710\folivora\util\plugin_base\IPluginBase;
+use pocketmine\Server;
 
-class Shop {
+class Shop implements IPluginBase {
 
-    public function init(PluginBase $base) : void {
+    public function onEnable(Server $server) : void {
         ItemShopAPI::getInstance()->init();
         EffectShopAPI::getInstance()->init();
         EnchantShopAPI::getInstance()->init();
-        $base->getServer()->getCommandMap()->registerAll('shopSystem', [
+        $server->getCommandMap()->registerAll('shopSystem', [
             new ItemShopCommand(),
             new EnchantShopCommand(),
             new EffectShopCommand(),
         ]);
+    }
+
+    public function onDisable(Server $server) : void {
     }
 }
