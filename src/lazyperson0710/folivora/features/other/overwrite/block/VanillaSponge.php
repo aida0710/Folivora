62dc0979dcc5737175f336434fe97cd71fbb14ab
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-06-02 19:58:28 +0900

    スポンジをpm5に対応

--- src/lazyperson0710/folivora/features/other/overwrite/block/VanillaSponge.php
+++ src/lazyperson0710/folivora/features/other/overwrite/block/VanillaSponge.php
@@ -4,12 +4,14 @@ declare(strict_types = 1);
 
 namespace lazyperson0710\folivora\features\other\overwrite\block;
 
+use lazyperson0710\folivora\features\world_manager\database\WorldCategory;
 use lazyperson0710\folivora\util\message\send_message\SendTip;
 use pocketmine\block\Block;
 use pocketmine\block\BlockBreakInfo;
 use pocketmine\block\BlockIdentifier;
-use pocketmine\block\BlockLegacyIds;
 use pocketmine\block\BlockToolType;
+use pocketmine\block\BlockTypeIds;
+use pocketmine\block\BlockTypeInfo;
 use pocketmine\block\Sponge;
 use pocketmine\block\VanillaBlocks;
 use pocketmine\item\Item;
@@ -17,10 +19,12 @@ use pocketmine\math\Vector3;
 use pocketmine\player\Player;
 use pocketmine\world\BlockTransaction;
 
-class VanillaSponge extends Sponge {
+class VanillaSponge extends Sponge
+{
 
-    public function __construct() {
-        parent::__construct(new BlockIdentifier(BlockLegacyIds::SPONGE, 0), 'Sponge', new BlockBreakInfo(0.6, BlockToolType::HOE));
+    public function __construct()
+    {
+        parent::__construct(new BlockIdentifier(BlockTypeIds::SPONGE), 'Sponge', new BlockTypeInfo(new  BlockBreakInfo(0.6, BlockToolType::HOE)));
     }
 
     public function place(BlockTransaction $tx, Item $item, Block $blockReplace, Block $blockClicked, int $face, Vector3 $clickVector, Player $player = null) : bool {
@@ -44,9 +48,9 @@ class VanillaSponge extends Sponge {
         for ($x = 0; $x <= 16; $x++) {
             for ($y = 0; $y <= 16; $y++) {
                 for ($z = 0; $z <= 16; $z++) {
-                    $id = $world->getBlockAt($startX + $x, $startY + $y, $startZ + $z)->getId();
-                    if ($id === BlockLegacyIds::WATER || $id === BlockLegacyIds::FLOWING_WATER || $id === BlockLegacyIds::LAVA || $id === BlockLegacyIds::FLOWING_LAVA) {
-                        $world->setBlockAt($startX + $x, $startY + $y, $startZ + $z, VanillaBlocks::AIR(), true);
+                    $id = $world->getBlockAt($startX + $x, $startY + $y, $startZ + $z)->getTypeId();
+                    if ($id === BlockTypeIds::WATER || $id === BlockTypeIds::WATER_CAULDRON || $id === BlockTypeIds::LAVA || $id === BlockTypeIds::LAVA_CAULDRON) {
+                        $world->setBlockAt($startX + $x, $startY + $y, $startZ + $z, VanillaBlocks::AIR());
                         $absorbed = true;
                     }
                 }
