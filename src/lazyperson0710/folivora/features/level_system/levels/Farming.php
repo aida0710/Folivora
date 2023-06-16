b77802d09c6b1f47b834ca654bbde3db885cc3aa
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-06-16 17:21:48 +0900

    レベルを三種類実装

---
+++ src/lazyperson0710/folivora/features/level_system/levels/Farming.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace lazyperson0710\folivora\features\level_system\levels;
+
+use lazyperson0710\folivora\features\level_system\util\LevelConfig;
+use pocketmine\utils\SingletonTrait;
+
+class Farming implements ILevel {
+
+    use SingletonTrait;
+
+    private const DEFAULT_LEVEL = 1;
+    private const DEFAULT_EXP = 1;
+    private const DEFAULT_LEVEL_UP_EXP = 250;
+
+    public const PATH = 'player/levels/farming_level.json';
+
+    /**
+     * @return LevelConfig
+     */
+    public function getConfig() : LevelConfig {
+        return new LevelConfig(self::PATH, $this);
+    }
+
+    /**
+     * @return int
+     */
+    public function getDefaultLevel() : int {
+        return self::DEFAULT_LEVEL;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDefaultExp() : int {
+        return self::DEFAULT_EXP;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDefaultExpToNextLevel() : int {
+        return self::DEFAULT_LEVEL_UP_EXP;
+    }
+
+}
