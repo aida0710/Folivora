23b697b53beae1abcdefa275d9d1355f8a936c8a
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-06-17 19:22:25 +0900

    テスト用にListenerに追加

--- src/lazyperson0710/folivora/features/level_system/listener/MiningLevelListener.php
+++ src/lazyperson0710/folivora/features/level_system/listener/MiningLevelListener.php
@@ -4,9 +4,13 @@ declare(strict_types = 1);
 
 namespace lazyperson0710\folivora\features\level_system\listener;
 
+use lazyperson0710\folivora\features\level_system\levels\Build;
+use lazyperson0710\folivora\features\level_system\levels\Farming;
 use lazyperson0710\folivora\features\level_system\levels\Mining;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerJumpEvent;
 
 class MiningLevelListener implements Listener {
 
@@ -18,7 +22,19 @@ class MiningLevelListener implements Listener {
         if ($event->isCancelled()) return;
         $player = $event->getPlayer();
         Mining::getInstance()->getConfig()->getFunction($player)->addExp(1);
-        var_dump(Mining::getInstance()->getConfig()->getFunction($player)->getExp());
+        var_dump('mining - ' . Mining::getInstance()->getConfig()->getFunction($player)->getExp());
+    }
+
+    public function onJump(PlayerJumpEvent $event) : void {
+        $player = $event->getPlayer();
+        Farming::getInstance()->getConfig()->getFunction($player)->addExp(1);
+        var_dump('farming - ' . Farming::getInstance()->getConfig()->getFunction($player)->getExp());
+    }
+
+    public function onInteract(PlayerInteractEvent $event) : void {
+        $player = $event->getPlayer();
+        Build::getInstance()->getConfig()->getFunction($player)->addExp(1);
+        var_dump('build - ' . Farming::getInstance()->getConfig()->getFunction($player)->getExp());
     }
 
 }
