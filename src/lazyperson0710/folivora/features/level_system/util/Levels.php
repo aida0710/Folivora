bd74e783a5c49d86c01db274f4d1a6640dbccc5d
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-06-21 09:15:41 +0900

    levelの列挙型を追加

---
+++ src/lazyperson0710/folivora/features/level_system/util/Levels.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace lazyperson0710\folivora\features\level_system\util;
+
+enum Levels : string {
+
+    case MINING = 'mining';
+    case FARMING = 'farming';
+    case BUILD = 'build';
+}
