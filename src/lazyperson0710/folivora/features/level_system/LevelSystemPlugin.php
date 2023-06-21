ead0e6195b4de1a8f236ae259bb22ebca245d330
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-06-21 09:16:22 +0900

    セーブを各々で行うように

--- src/lazyperson0710/folivora/features/level_system/LevelSystemPlugin.php
+++ src/lazyperson0710/folivora/features/level_system/LevelSystemPlugin.php
@@ -5,6 +5,8 @@ declare(strict_types = 1);
 namespace lazyperson0710\folivora\features\level_system;
 
 use JsonException;
+use lazyperson0710\folivora\features\level_system\levels\Build;
+use lazyperson0710\folivora\features\level_system\levels\Farming;
 use lazyperson0710\folivora\features\level_system\levels\Mining;
 use lazyperson0710\folivora\features\level_system\listener\MiningLevelListener;
 use lazyperson0710\folivora\util\plugin_base\IPluginBase;
@@ -26,8 +28,28 @@ class LevelSystemPlugin implements IPluginBase {
      * @throws JsonException
      */
     public function onEnable(Server $server) : void {
-        Mining::getInstance()->getConfig()->registerConfigClass();
+        $this->registerConfigClass();
+        $this->createConfig();
         RegisterListener::register(new MiningLevelListener());
+    }
+
+    /**
+     * @return void
+     */
+    private function registerConfigClass() : void {
+        Mining::getInstance()->getConfig()->registerConfigClass();
+        Farming::getInstance()->getConfig()->registerConfigClass();
+        Build::getInstance()->getConfig()->registerConfigClass();
+    }
+
+    /**
+     * @return void
+     * @throws JsonException
+     */
+    private function createConfig() : void {
+        Mining::getInstance()->getConfig()->createConfigFile();
+        Farming::getInstance()->getConfig()->createConfigFile();
+        Build::getInstance()->getConfig()->createConfigFile();
     }
 
 }
