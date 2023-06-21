e5ae6d90f23f6fe549a57d0fcd962bd1ba769d82
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-06-21 09:19:54 +0900

    データを正常にセーブできるように

--- src/lazyperson0710/folivora/features/level_system/util/LevelConfig.php
+++ src/lazyperson0710/folivora/features/level_system/util/LevelConfig.php
@@ -5,7 +5,6 @@ declare(strict_types = 1);
 namespace lazyperson0710\folivora\features\level_system\util;
 
 use JsonException;
-use lazyperson0710\folivora\features\level_system\levels\ILevel;
 use lazyperson0710\folivora\util\config\ConfigFoundation;
 use lazyperson0710\folivora\util\config\exception\ConfigSaveException;
 use lazyperson0710\folivora\util\config\IConfig;
@@ -14,41 +13,45 @@ use pocketmine\utils\Config;
 
 class LevelConfig implements IConfig {
 
+    /** @var Config[] */
+    private static array $config;
+    private static array $cache;
+
     /**
-     * @param ILevel $ILevel
+     * @param Levels $Level
      * @param string $path
-     * @param Config $config
-     * @param array  $cache
      */
     public function __construct(
-        private readonly Ilevel $ILevel,
+        private readonly Levels $Level,
         private readonly string $path,
-        private readonly Config $config,
-        private array $cache,
     ) {
     }
 
     /**
+     * 基本的には特殊なことがない限り一度しか呼び出されない
+     *
      * @return void
      * @throws JsonException
+     * @see LevelConfig::registerConfigClass()
+     *
      */
     public function createConfigFile() : void {
+        if (isset(self::$config[$this->Level->value])) return;
         try {
-            $this->config = ConfigFoundation::createConfigFile($this->path);
-            $this->cache = $this->config->getAll();
-            $this->ILevel->setConfig($this->config);
+            self::$config[$this->Level->value] = ConfigFoundation::createConfigFile($this->path);
+            self::$cache[$this->Level->value] = self::$config[$this->Level->value]->getAll();
         } catch (ConfigSaveException $exception) {
             throw new ConfigSaveException($exception->getMessage());
         }
     }
 
     /**
+     * 起動時に一度呼び出してください
+     *
      * @return void
-     * @throws JsonException
      */
     public function registerConfigClass() : void {
         ConfigFoundation::registerConfigClass($this);
-        $this->createConfigFile();
     }
 
     /**
@@ -56,12 +59,13 @@ class LevelConfig implements IConfig {
      * @throws JsonException
      */
     public function runSave() : void {
-        $this->config->setAll($this->cache);
-        $this->config->save();
+        var_dump($this->Level->value);
+        self::$config[$this->Level->value]->setAll(self::$cache[$this->Level->value]);
+        self::$config[$this->Level->value]->save();
     }
 
     public function getFunction(Player $player) : LevelFoundation {
-        return new LevelFoundation($player, self::$cache, $this->iLevelClass);
+        return new LevelFoundation($player, self::$cache[$this->Level->value], $this->Level);
     }
 
 }
