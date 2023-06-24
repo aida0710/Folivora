bba703bfa1fecdacfccca1e1eae7c18fdfc199ca
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-06-24 18:40:20 +0900

    テスト機能の為デバックに追加

--- src/lazyperson0710/folivora/features/debug/test/listener/LEvent.php
+++ src/lazyperson0710/folivora/features/debug/test/listener/LEvent.php
@@ -4,26 +4,45 @@ declare(strict_types = 1);
 
 namespace lazyperson0710\folivora\features\debug\test\listener;
 
-use lazyperson0710\folivora\features\settings\Setting;
-use lazyperson0710\folivora\features\settings\setting_type\normal\JoinItemsSetting;
+use lazyperson0710\folivora\features\level_system\levels\Build;
+use lazyperson0710\folivora\features\level_system\levels\Farming;
+use lazyperson0710\folivora\features\level_system\levels\Mining;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerJumpEvent;
 
 class LEvent implements Listener {
 
     /**
-     *
-     * @param PlayerJoinEvent $event
+     * @param BlockBreakEvent $event
      * @return void
      */
-    public function onJoin(PlayerJoinEvent $event): void {
-        var_dump(Setting::getInstance()->getSettingData($event->getPlayer(), new JoinItemsSetting()));
+    public function onBreakBlock(BlockBreakEvent $event): void {
+        if ($event->isCancelled()) return;
+        $player = $event->getPlayer();
+        Mining::getInstance()->getConfig()->getFunction($player)->addExp(1);
+        var_dump('mining - ' . Mining::getInstance()->getConfig()->getFunction($player)->getExp());
     }
 
-    public function onBreak(BlockBreakEvent $event): void {
-        Setting::getInstance()->editSettingData($event->getPlayer(), new JoinItemsSetting(), false);
-        var_dump(Setting::getInstance()->getSettingData($event->getPlayer(), new JoinItemsSetting()));
+    /**
+     * @param PlayerJumpEvent $event
+     * @return void
+     */
+    public function onJump(PlayerJumpEvent $event): void {
+        $player = $event->getPlayer();
+        Farming::getInstance()->getConfig()->getFunction($player)->addExp(1);
+        var_dump('farming - ' . Farming::getInstance()->getConfig()->getFunction($player)->getExp());
+    }
+
+    /**
+     * @param PlayerInteractEvent $event
+     * @return void
+     */
+    public function onInteract(PlayerInteractEvent $event): void {
+        $player = $event->getPlayer();
+        Build::getInstance()->getConfig()->getFunction($player)->addExp(1);
+        var_dump('build - ' . Build::getInstance()->getConfig()->getFunction($player)->getExp());
     }
 
 }
