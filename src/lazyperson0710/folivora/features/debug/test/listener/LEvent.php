301f563719561605c8ca369deb605a57af8216da
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-07-01 14:34:14 +0900

    ログイン時にdiscordメッセージを送るように

--- src/lazyperson0710/folivora/features/debug/test/listener/LEvent.php
+++ src/lazyperson0710/folivora/features/debug/test/listener/LEvent.php
@@ -4,15 +4,37 @@ declare(strict_types = 1);
 
 namespace lazyperson0710\folivora\features\debug\test\listener;
 
+use bbo51dog\pmdiscord\connection\Webhook;
+use bbo51dog\pmdiscord\element\Embed;
+use bbo51dog\pmdiscord\element\Embeds;
+use DateTime;
+use DateTimeInterface;
 use lazyperson0710\folivora\features\level_system\levels\Build;
 use lazyperson0710\folivora\features\level_system\levels\Farming;
 use lazyperson0710\folivora\features\level_system\levels\Mining;
+use lazyperson0710\folivora\util\webhook\WebHookURL;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerJumpEvent;
 
 class LEvent implements Listener {
+
+    public function onJoin(PlayerJoinEvent $event): void {
+        $webhook = Webhook::create(WebHookURL::getInstance()->getURL('debug'));
+        $webhook->setCustomName('setCustomName');
+        $webhook->setCustomAvatar('https://lazyperson0710.tech/assets/profile/icon/neko.jpg');
+        $embed = (new Embed())
+            ->setTitle('title')
+            ->setColor(13421619)
+            ->setAuthorName('AuthorName')
+            ->setTime((new DateTime())->format(DateTimeInterface::ATOM));
+        $embeds = new Embeds();
+        $embeds->add($embed);
+        $webhook->add($embeds);
+        $webhook->send();
+    }
 
     /**
      * @param BlockBreakEvent $event
